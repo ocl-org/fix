86e7686916a063082111be1b4831c201f24f8adf
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-25 13:00:43 +0100

    feat: add fix.hpp for useability.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ocl/fix.hpp
@@ -0,0 +1,26 @@
+/*
+ * File: fix.hpp
+ * Purpose: Financial Information Exchange function and headers in C++
+ * Author: Amlal El Mahrouss (amlal@nekernel.org)
+ * Copyright 2025, Amlal El Mahrouss, licensed under the Boost Software License.
+ */
+
+#ifndef OCL_FIX_HPP
+#define OCL_FIX_HPP
+
+#include <ocl/fix/detail/config.hpp>
+#include <ocl/fix/checksum.hpp>
+#include <ocl/fix/parser.hpp>
+
+namespace ocl::fix
+{
+    
+    template <typename It, class Pred>
+	It find(It begin, It end, Pred callable);
+
+	template <typename It, class Callable>
+	It find_if(It begin, It end, Callable callable);
+
+}
+
+#endif
