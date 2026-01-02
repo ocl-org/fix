d0decd01ec7c9e3f3d493cc6aa9ec87fd34a6722
Amlal El Mahrouss <amlal@nekernel.org>
2026-01-02 09:33:09 +0100

    feat: final API implementation of free functions in progress.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/ocl/fix.hpp
+++ include/ocl/fix.hpp
@@ -21,6 +21,12 @@ namespace ocl::fix
 	template <typename It, class Pred>
 	Pred find_if(It begin, It end, Pred callable);
 
+	template <typename It, class Pred>
+	typename Pred::size_type erase(It begin, It end, Pred callable);
+
+	template <typename It, class Pred>
+	typename Pred::size_type erase_if(It begin, It end, Pred callable);
+
 } // namespace ocl::fix
 
 #endif
